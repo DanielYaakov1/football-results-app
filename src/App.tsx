e53358a5b1fa367a5cc82e7539e2f9c9fe5e5f42
DanielYaako1 <daniely@moveo.co.il>
2023-05-20 13:29:27 +0300

    add: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './pages/home-page';
+
+function App() {
+     return (
+          <div className='App'>
+               <header className='App-header'>HEADER</header>
+               <HomePage />
+          </div>
+     );
+}
+
+export default App;
